1441f9bc912b1479c4d16c6937d024276a058013
Trinh Viet Duong <vduong6a@gmail.com>
2025-12-02 13:10:16 +0700

    Remove missing css import

--- src/Plants.jsx
+++ src/Plants.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import './ProductList.css';
 import CartItem from './CartItem';
 import { useSelector, useDispatch } from 'react-redux';
 import { addItem } from './redux/CartSlice';
